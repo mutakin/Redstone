a6e0675402db838ccfa3ae3fbd2fe2cfaf9eae03
mutakin <mutakin@gmail.com>
2018-11-09 17:43:31 +0700

    First Commit

---
+++ index.php
@@ -0,0 +1,110 @@
+<!DOCTYPE html>
+<html lang="en">
+  <head>
+    <!-- Required meta tags -->
+    <meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no">
+
+    <!-- Bootstrap CSS -->
+    <!-- <link rel="stylesheet" href="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/css/bootstrap.min.css" integrity="sha384-MCw98/SFnGE8fJT3GXwEOngsV7Zt27NXFoaoApmYm81iuXoPkFOJwJ8ERdknLPMO" crossorigin="anonymous"> -->
+    <link rel="stylesheet" href="bootstrap/dist/css/bootstrap.min.css">
+    <link rel="stylesheet" href="css-self/styles-self.css">
+    <title>Redstone | Mie Ayam & Mie Bakso</title>
+  </head>
+  <body>
+    <div class="jumbotron-fluid mb-5">
+        <!-- create navbar -->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-dark fixed-top">
+            <!-- container -->
+            <div class="container">
+                <!-- Navbar Brand -->
+                <a class="navbar-brand text-white font-weight-bold text-uppercase" href="#">Redstone</a>
+                <!-- Button Collapse -->
+                <button class="navbar-toggler" type="button" data-toggle="collapse" data-target="#navbarNavAltMarkup" aria-controls="navbarNavAltMarkup" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                </button>
+                <div class="collapse navbar-collapse" id="navbarNavAltMarkup">
+                    <div class="navbar-nav ml-auto mr-0">
+                        <a class="nav-item nav-link text-white font-weight-bold ml-3" href="#">Home</a>
+                        <a class="nav-item nav-link text-white font-weight-bold ml-3" href="#">Menu Favorit</a>
+                        <a class="nav-item nav-link text-white font-weight-bold ml-3" href="#">Menu Makanan</a>
+                        <a class="nav-item nav-link text-white font-weight-bold ml-3" href="#">Koki</a>
+                        <a class="nav-item nav-link text-white font-weight-bold ml-3" href="#">Kontak</a>
+                    </div>
+                </div>    
+            </div>
+            
+        </nav>
+
+        <article class="container">
+            <h1 class="heading-text">Selamat Datang Di Redstone</h1>
+            <a href="#" class="btn btn-danger btn-lg button-cek-menu">Cek Menu</a>
+        </article>
+
+    </div>
+
+
+    <main>
+        <article class="container">
+            <h3 class="text-center">Menu Favorit</h3>
+            <hr style="width: 250px;border-color: red;">
+            <section>
+                <div class="row">
+                    <div class="col-lg">
+                        <img src="images/d2.jpg" alt="menu favorit" width="365" height="346" class="img-thumbnail">
+                        <h5 class="text-center">MIE AYAM ORIGINAL</h5>
+                        <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim</p>
+                    </div>
+                    <div class="col-lg">
+                        <img src="images/d2.jpg" alt="menu favorit" width="365" height="346" class="img-thumbnail">
+                        <h5 class="text-center">MIE BAKSO ORIGINAL</h5>
+                        <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim</p>
+                    </div>
+                    <div class="col-lg">
+                        <img src="images/d2.jpg" alt="menu favorit" width="365" height="346" class="img-thumbnail">
+                        <h5 class="text-center">MIE AYAM SPESIAL</h5>
+                        <p class="text-center">Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod
+                        tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim</p>
+                    </div>
+                </div>
+            </section>
+        </article>
+
+        <article class="container-fluid bg-light">
+            <h3 class="text-center">Menu Makanan</h3>
+            <hr style="width: 250px;border-color: red;">
+            <section class="container">
+                <div id="carouselExampleFade" class="carousel slide carousel-fade" data-ride="carousel">
+                  <div class="carousel-inner">
+                    <div class="carousel-item active">
+                      <img class="d-block w-100" src="images/slider1.jpg" alt="First slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/slider1.jpg" alt="Second slide">
+                    </div>
+                    <div class="carousel-item">
+                      <img class="d-block w-100" src="images/slider1.jpg" alt="Third slide">
+                    </div>
+                  </div>
+                  <a class="carousel-control-prev" href="#carouselExampleFade" role="button" data-slide="prev">
+                    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Previous</span>
+                  </a>
+                  <a class="carousel-control-next" href="#carouselExampleFade" role="button" data-slide="next">
+                    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+                    <span class="sr-only">Next</span>
+                  </a>
+                </div>
+            </section>
+        </article>
+    </main>
+    
+    <!-- Optional JavaScript -->
+    <!-- jQuery first, then Popper.js, then Bootstrap JS -->
+    <script src="https://code.jquery.com/jquery-3.3.1.slim.min.js" integrity="sha384-q8i/X+965DzO0rT7abK41JStQIAqVgRVzpbzo5smXKp4YfRvH+8abtTE1Pi6jizo" crossorigin="anonymous"></script>
+    <script src="https://cdnjs.cloudflare.com/ajax/libs/popper.js/1.14.3/umd/popper.min.js" integrity="sha384-ZMP7rVo3mIykV+2+9J3UJ46jBk0WLaUAdn689aCwoqbBJiSnjAK/l8WvCWPIPm49" crossorigin="anonymous"></script>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.1.3/js/bootstrap.min.js" integrity="sha384-ChfqqxuZUCnJSK3+MXmPNIyE6ZbWh2IMqE241rYiqJxyMiZ6OW/JmZQ5stwEULTy" crossorigin="anonymous"></script>
+  </body>
+</html>
